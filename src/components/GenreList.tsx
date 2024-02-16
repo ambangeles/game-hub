94278ffe6c03df578d33dff6109364068d42e4fd
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-16 08:55:07 +0800

    Update grid spacing and padding, add genre images

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,30 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-    const { data } = useGenres();
-    return data.map((genre) => <li key={genre.id}>{genre.name}</li>);
+    const { data, isLoading, error } = useGenres();
+
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    if (error) {
+        return null;
+    }
+
+    return (
+        <List>
+            {data.map((genre) => (
+                <ListItem key={genre.id} paddingY="5px">
+                    <HStack>
+                        <Image boxSize={"32px"} borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                        <Text fontSize={"large"}>{genre.name}</Text>
+                    </HStack>
+                </ListItem>
+            ))}
+        </List>
+    );
 };
 
 export default GenreList;
