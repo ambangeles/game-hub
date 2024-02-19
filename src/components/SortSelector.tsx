ca460934e5d6796ab7085f6dd1ba9c6dc1d5e744
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-19 13:53:33 +0800

    Add sort order functionality to SortSelector component

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,21 +1,49 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+    onSelectSortOrder: (order: string) => void;
+    sortOrder: string;
+}
 
+const SortSelector = ({ onSelectSortOrder, sortOrder}: Props) => {
+    const sortOrders = [
+        {
+            value: "",
+            label: "Relevance",
+        },
+        {
+            value: "-added",
+            label: "Date added",
+        },
+        {
+            value: "name",
+            label: "Name",
+        },
+        {
+            value: "-released",
+            label: "Release date",
+        },
+        {
+            value: "-metacritic",
+            label: "Popularity",
+        },
+        {
+            value: "-rating",
+            label: "Average rating",
+        },
+    ]
     return (
         <Menu>
             <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-                "Order by relevance"
+                Order by: {sortOrders.find((order) => order.value === sortOrder)?.label || "Relevance"}
             </MenuButton>
             <MenuList>
-                {/* {isLoading && <MenuItem>Loading...</MenuItem>}
-                {error && <MenuItem>{error}</MenuItem>}
-                {!isLoading &&
-                    data.map((platform) => (
-                        <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>
-                    ))} */}
-                    <MenuItem>123</MenuItem>
+            {
+                sortOrders.map((order) => (
+                    <MenuItem  onClick={() => onSelectSortOrder(order.value)} key={order.value}>{order.label}</MenuItem>
+                ))
+            }
             </MenuList>
         </Menu>
     );
