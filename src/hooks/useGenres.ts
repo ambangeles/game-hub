554a890e6416d00c4f1fe8e25be0d41a95470168
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-21 21:43:04 +0800

    Refactor initialData in useGenres and usePlatforms hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -15,7 +15,7 @@ const useGenres = () =>
         queryKey: ["genres"],
         queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000,
-        initialData: { results: genres, count: genres.length},
+        initialData: genres,
     });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ const usePlatforms = () =>
         queryKey: ["platforms"],
         queryFn: apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000,
-        initialData: { results: platforms, count: platforms.length },
+        initialData: platforms,
     });
 
 export default usePlatforms;
