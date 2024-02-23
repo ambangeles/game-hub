991250f54e2bd2c1e95e989f7dd5282076ac6743
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-23 23:30:23 +0800

    Add dependencies and update components with zustand

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,11 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { useState } from "react";
 import PlatformSelector from "./components/PlatformSelector";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
-export interface GameQuery {
-    genreId?: number;
-    platformId?: number | null;
-    sortOrder: string;
-    searchText?: string;
-}
-
 function App() {
-    const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
     return (
         <Grid
@@ -36,43 +27,24 @@ function App() {
             }}
         >
             <GridItem area={"nav"}>
-                <NavBar
-                    onSearch={(searchText) =>
-                        setGameQuery({ ...gameQuery, searchText })
-                    }
-                />
+                <NavBar />
             </GridItem>
             <Show above="lg">
                 <GridItem area={"aside"} paddingX={5}>
-                    <GenreList
-                        onSelectGenre={(genre) =>
-                            setGameQuery({ ...gameQuery, genreId: genre.id })
-                        }
-                        selectedGenreId={gameQuery?.genreId || null}
-                    />
+                    <GenreList />
                 </GridItem>
             </Show>
             <GridItem area={"main"}>
                 <Box paddingLeft={2}>
-                    <GameHeading gameQuery={gameQuery} />
+                    <GameHeading />
                     <Flex marginBottom={5}>
                         <Box marginRight={5}>
-                            <PlatformSelector
-                                onSelectPlatform={(platform) =>
-                                    setGameQuery({ ...gameQuery, platformId: platform.id })
-                                }
-                                selectedPlatformId={gameQuery?.platformId || null}
-                            />
+                            <PlatformSelector />
                         </Box>
-                        <SortSelector
-                            sortOrder={gameQuery.sortOrder}
-                            onSelectSortOrder={(sortOrder) =>
-                                setGameQuery({ ...gameQuery, sortOrder })
-                            }
-                        />
+                        <SortSelector />
                     </Flex>
                 </Box>
-                <GameGrid gameQuery={gameQuery} />
+                <GameGrid />
             </GridItem>
         </Grid>
     );
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,16 +7,14 @@ import {
     ListItem,
     Spinner,
 } from "@chakra-ui/react";
-import useGenres, { Genre } from "../hooks/useGenres";
+import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import useGameQueryStore from "../store";
 
-interface Props {
-    onSelectGenre: (genre: Genre) => void;
-    selectedGenreId: number | null;
-}
-
-const GenreList = ({ onSelectGenre, selectedGenreId }: Props) => {
+const GenreList = () => {
     const { data, isLoading, error } = useGenres();
+    const selectedGenreId = useGameQueryStore(s => s.gameQuery.genreId);
+    const setSelectedGenreId = useGameQueryStore(s => s.setGenreId);
 
     if (isLoading) {
         return <Spinner />;
@@ -42,7 +40,7 @@ const GenreList = ({ onSelectGenre, selectedGenreId }: Props) => {
                                 objectFit={"cover"}
                             />
                             <Button
-                                onClick={() => onSelectGenre(genre)}
+                                onClick={() => setSelectedGenreId(genre.id)}
                                 fontSize={"large"}
                                 variant={"link"}
                                 fontWeight={
