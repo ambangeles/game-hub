e1bf057ef127c02f35fde4f369034b30827227bb
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-24 17:54:43 +0800

    Update import paths for entities

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,18 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Platform } from "./usePlatforms";
 import APIClient, { FetchResponse } from "../services/api-client";
 import ms from "ms";
 import useGameQueryStore from "../store";
-
-export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-    slug: string;
-    description_raw: string;
-}
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 
