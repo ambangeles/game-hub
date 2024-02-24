3c355d844ed7e7aa1ccf47c052d3845c3b05ac6b
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-24 18:16:32 +0800

    Add Genre and Publisher imports, and update Game interface

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
     id: number;
     name: string;
     background_image: string;
-    parent_platforms: { platform: Platform; }[];
+    parent_platforms: { platform: Platform }[];
     metacritic: number;
     slug: string;
     description_raw: string;
+    genres: Genre[];
+    publishers: Publisher[];
 }
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,19 +2,23 @@ import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailPage = () => {
     const { slug } = useParams();
-    const {data: game, isLoading, error } = useGame(slug!);
+    const { data: game, isLoading, error } = useGame(slug!);
 
-  if (isLoading) <Spinner />;
+    if (isLoading) <Spinner />;
 
-  if (error) throw error;
+    if (error) throw error;
 
-    return <>
-      <Heading>{game?.name}</Heading>
-      <ExpandableText>{game?.description_raw}</ExpandableText> 
-    </>;
+    return (
+        <>
+            <Heading>{game?.name}</Heading>
+            <ExpandableText>{game?.description_raw || ""}</ExpandableText>
+            <GameAttributes game={game!} />
+        </>
+    );
 };
 
 export default GameDetailPage;
