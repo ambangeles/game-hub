390a61a74037c3cbf7e3f6084f1477ce077b9bd9
Alfonso Martin Angeles <alfonsomartinangeles@gmail.com>
2024-02-24 18:47:46 +0800

    Refactor NavBar and SearchInput components, add grid layout to GameDetailPage

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
@@ -17,13 +17,23 @@ const GameDetailPage = () => {
     if (!game) return null;
 
     return (
-        <>
-            <Heading>{game?.name}</Heading>
-            <ExpandableText>{game?.description_raw || ""}</ExpandableText>
-            <GameAttributes game={game} />
-            <GameTrailer gameId={game.id} />
-            <GameScreenshots gameId={game.id} />
-        </>
+        <SimpleGrid
+            columns={{
+                base: 1,
+                md: 2,
+            }}
+            spacing={4}
+        >
+            <GridItem>
+                <Heading>{game?.name}</Heading>
+                <ExpandableText>{game?.description_raw || ""}</ExpandableText>
+                <GameAttributes game={game} />
+            </GridItem>
+            <GridItem>
+                <GameTrailer gameId={game.id} />
+                <GameScreenshots gameId={game.id} />
+            </GridItem>
+        </SimpleGrid>
     );
 };
 
